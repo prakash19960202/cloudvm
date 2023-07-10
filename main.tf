9162f2d4576db0600937dbd8fa7c2894307fb773
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-10 16:19:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "google_compute_instance" "cloudvm1" {
+  attached_disk {
+    device_name = var.disk
+    mode        = "READ_WRITE"
+    source      = "projects/groovy-karma-388506/zones/us-central1-a/disks/disk-3"
+    }
+  boot_disk {
+    auto_delete = true
+    device_name = var.device_name
+
+    initialize_params {
+      image = "projects/groovy-karma-388506/global/images/cloudvm1"
+      size  = 50
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = var.machine
+  name         = var.name
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/groovy-karma-388506/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = var.service_account
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-central1-a"
+}
