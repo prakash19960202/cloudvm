4bee0f6fec7ff5416f15c94105f4d69864b75b8e
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-10 16:20:31 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "service_account" {
+  type        = string
+  default     = "101674418114-compute@developer.gserviceaccount.com"
+}
+variable "disk" {
+  type        = string
+  default     = "disk-3"
+}
+variable "device_name" {
+  type        = string
+  default     = "cloudvm1"
+}
+variable "machine" {
+  type        = string
+  default     = "e2-medium"
+}
+variable "name" {
+  type        = string
+  default     = "cloudvm1"
+}
